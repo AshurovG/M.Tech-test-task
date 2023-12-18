504e5859dc38e357d7983722d1feb7c238897965
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2023-12-18 03:38:52 +0300

    init project and install necessary packages

---
+++ src/App/App.tsx
@@ -0,0 +1,23 @@
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import './App.module.scss'
+import MainPage from 'pages/MainPage'
+import UsersPage from 'pages/UsersPage'
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+          <Routes>
+              <Route path='/' element={<MainPage/>}/>
+              <Route path='/users' element={<UsersPage/>}/>
+              <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        {/* </div>} */}
+      </HashRouter>
+      <ToastContainer autoClose={1500} pauseOnHover={false} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/UsersPage/UsersPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './UsersPage.module.scss'
+
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
