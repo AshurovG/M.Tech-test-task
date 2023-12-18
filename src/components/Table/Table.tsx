b61383009850044e12fefffbb778310c0ca4aa42
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2023-12-18 15:46:25 +0300

    create users page

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,33 +1,38 @@
 import React from 'react'
 import styles from './Table.module.scss'
 import { useFileData } from 'slices/MainSlice'
+import cn from 'classnames'
 
-const Table = () => {
+export type TableProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    className?: string;
+};
+
+const Table: React.FC<TableProps> = ({className}) => {
     const fileData = useFileData()
 
     return (
-        <table className={styles.table}>
-        <thead className={styles.table__header}>  {/*К сожалению, здесь с файла приходят заголовки на английском
-        в отличие от макета, и поле day почему то приходит как  "bday" :-)*/}
-            <tr className={styles['table__header-items']}>
-                {fileData && fileData[0].map((item, index) => (
-                    <th key={index} className={styles['table__header-item']}>{item}</th>
+        <table className={cn(styles.table, className)}>
+            <thead className={styles.table__header}>  {/*К сожалению, здесь с файла приходят заголовки на английском
+            в отличие от макета, и поле day почему то приходит как  "bday" :-)*/}
+                <tr className={styles['table__header-items']}>
+                    {fileData && fileData[0].map((item, index) => (
+                        <th key={index} className={styles['table__header-item']}>{item}</th>
+                    ))}
+                </tr>
+            </thead>
+
+
+            <tbody>
+                {fileData?.slice(1).map((row, index) => (
+                <tr key={index}>
+                    <td className={styles['table__body-item']}>{row[0]}</td>
+                    <td className={styles['table__body-item']}>{row[1]}</td>
+                    <td className={styles['table__body-item']}>{row[2]}</td>
+                    <td className={styles['table__body-item']}>{row[3]}</td>
+                    <td className={styles['table__body-item']}>{row[4]}</td>
+                </tr>
                 ))}
-            </tr>
-        </thead>
-
-
-        <tbody>
-            {fileData?.slice(1).map((row, index) => (
-            <tr key={index}>
-                <td className={styles['table__body-item']}>{row[0]}</td>
-                <td className={styles['table__body-item']}>{row[1]}</td>
-                <td className={styles['table__body-item']}>{row[2]}</td>
-                <td className={styles['table__body-item']}>{row[3]}</td>
-                <td className={styles['table__body-item']}>{row[4]}</td>
-            </tr>
-            ))}
-        </tbody>
+            </tbody>
         </table>
     )
 }
--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './UsersPage.module.scss'
 import { useFileData } from 'slices/MainSlice'
 import Table from 'components/Table'
+import Button from 'components/Button'
 
 const UsersPage = () => {
   const fileData = useFileData()
@@ -10,8 +11,11 @@ const UsersPage = () => {
     console.log(fileData)
   }, [])
   return (
-    <div>
-        <Table></Table>
+    <div className={styles.users__page}>
+        <div className={styles['users__page-header']}>
+          <Button className={styles['users__page-btn']}>Загрузить новый файл</Button>
+        </div>
+        <Table className={styles['users__page-table']}/>
     </div>
   )
 }
